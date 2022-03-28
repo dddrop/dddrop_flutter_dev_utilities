f9bb94868ae38c9a6f3dcef6419c345f838e5902
dddrop <go@dddrop.dev>
2022-03-28 21:36:38 +0900

    Fix export

--- lib/dddrop_flutter_dev_utilities.dart
+++ lib/dddrop_flutter_dev_utilities.dart
@@ -1,7 +1,3 @@
 library dddrop_flutter_dev_utilities;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'logger/provider_debugger.dart';
