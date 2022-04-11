c79c80da7f37926d339990a12b1d2070632275d4
dddrop <go@dddrop.dev>
2022-04-11 12:19:59 +0900

    Extension on `dartz`

---
+++ lib/extensions/dartz.dart
@@ -0,0 +1,50 @@
+import 'package:dartz/dartz.dart';
+
+extension EitherExtension<L extends Object, R> on Either<L, R> {
+  Left<L, Never> asLeft() {
+    return fold(
+      (L l) => Left<L, Never>(l),
+      (R r) => throw AssertionError('asLeft() is called on Right'),
+    );
+  }
+
+  Right<Never, R> asRight() {
+    return fold(
+      (L l) => throw AssertionError('asRight() is called on Left'),
+      (R r) => Right<Never, R>(r),
+    );
+  }
+
+  R? getOrNull() => fold((L l) => null, id);
+}
+
+extension EitherExceptionExtension<L extends Exception, R> on Either<L, R> {
+  void throwIfLeft() => fold((L l) => throw l, (R r) {});
+
+  R getOrThrow() => fold((L l) => throw l, (R r) => r);
+}
+
+extension FutureEitherExtension<L extends Exception, R>
+    on Future<Either<L, R>> {
+  Future<void> throwIfLeft() =>
+      then((Either<L, R> either) => either.throwIfLeft());
+
+  Future<R> getOrThrow() => then((Either<L, R> either) => either.getOrThrow());
+}
+
+Either<Exception, A> catchingException<A>(Function0<A> f) {
+  try {
+    return right(f());
+  } on Exception catch (e) {
+    return left(e);
+  }
+}
+
+Future<Either<Exception, A>> catchingExceptionFuture<A>(
+    Function0<Future<A>> f) async {
+  try {
+    return right(await f());
+  } on Exception catch (e) {
+    return left(e);
+  }
+}
