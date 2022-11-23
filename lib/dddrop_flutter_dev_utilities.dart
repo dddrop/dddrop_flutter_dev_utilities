c244cbc4642498a54a3f660bb8e46ca1051ef16e
dddrop <go@dddrop.dev>
2022-11-23 09:53:42 +0900

    Add a export

--- lib/dddrop_flutter_dev_utilities.dart
+++ lib/dddrop_flutter_dev_utilities.dart
@@ -3,6 +3,7 @@ library dddrop_flutter_dev_utilities;
 export 'extensions/build_context.dart';
 export 'extensions/color.dart';
 export 'extensions/dartz.dart';
+export 'extensions/generics.dart';
 export 'extensions/go_router.dart';
 export 'extensions/string.dart';
 export 'extensions/use_async_effect.dart';
