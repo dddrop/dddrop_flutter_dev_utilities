c7cc9123878e4bdd255d6d4454664a947ef889fa
dddrop <go@dddrop.dev>
2023-01-19 00:39:27 +0900

    Add a new future extension

---
+++ lib/extensions/future.dart
@@ -0,0 +1,12 @@
+extension FutureX<T> on Future<T> {
+  static Future<T> waitForAnimationDelay<T>({
+    required int milliseconds,
+    required Future<T> future,
+  }) async {
+    return (await Future.wait(<Future<T>>[
+      future,
+      Future<T>.delayed(Duration(milliseconds: milliseconds)),
+    ]))
+        .first;
+  }
+}
