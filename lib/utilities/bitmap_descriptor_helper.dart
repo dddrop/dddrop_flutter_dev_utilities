60ac698f49b085a1d08cfcc126ea9f3379903f60
dddrop <go@dddrop.dev>
2023-02-06 22:15:31 +0900

    Migrate `flutter_svg` from `1.1.6` to `2.0.0`

--- lib/utilities/bitmap_descriptor_helper.dart
+++ lib/utilities/bitmap_descriptor_helper.dart
@@ -1,5 +1,3 @@
-import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -11,76 +9,18 @@ class BitmapDescriptorHelper {
   static Future<BitmapDescriptor> getBitmapDescriptorFromSvgAsset(
     BuildContext context,
     String svgAssetLink,
-    Size size,
   ) async {
-    final ui.Image svgImage =
-        await _getSvgImageFromAssets(context, svgAssetLink);
-    final ui.Image sizedSvgImage = await _getSizedSvgImage(svgImage, size);
-
-    final ByteData? pngSizedBytes =
-        await sizedSvgImage.toByteData(format: ui.ImageByteFormat.png);
-    final Uint8List? unit8List = pngSizedBytes?.buffer.asUint8List();
-    if (unit8List == null) {
-      throw Exception('Uint8List is null');
-    }
-    return BitmapDescriptor.fromBytes(unit8List);
+    return SvgAssetLoader(svgAssetLink).loadBytes(context).then(
+        (ByteData value) =>
+            BitmapDescriptor.fromBytes(value.buffer.asUint8List()));
   }
 
   static Future<BitmapDescriptor> getBitmapDescriptorFromSvgString(
-    String svgString,
-    Size size,
-  ) async {
-    final ui.Image svgImage = await _getSvgImageFromString(svgString);
-    final ui.Image sizedSvgImage = await _getSizedSvgImage(svgImage, size);
-
-    final ByteData? pngSizedBytes =
-        await sizedSvgImage.toByteData(format: ui.ImageByteFormat.png);
-    final Uint8List? unit8List = pngSizedBytes?.buffer.asUint8List();
-    if (unit8List == null) {
-      throw Exception('Uint8List is null');
-    }
-    return BitmapDescriptor.fromBytes(unit8List);
-  }
-
-  static Future<ui.Image> _getSvgImageFromAssets(
     BuildContext context,
-    String svgAssertLink,
-  ) async {
-    final String svgString =
-        await DefaultAssetBundle.of(context).loadString(svgAssertLink);
-    final DrawableRoot drawableRoot = await svg.fromSvgString(svgString, '');
-    final ui.Picture picture = drawableRoot.toPicture();
-    final ui.Image image = await picture.toImage(
-        drawableRoot.viewport.width.toInt(),
-        drawableRoot.viewport.height.toInt());
-    return image;
-  }
-
-  static Future<ui.Image> _getSvgImageFromString(String svgString) async {
-    final DrawableRoot drawableRoot = await svg.fromSvgString(svgString, '');
-    final ui.Picture picture = drawableRoot.toPicture();
-    final ui.Image image = await picture.toImage(
-        drawableRoot.viewport.width.toInt(),
-        drawableRoot.viewport.height.toInt());
-    return image;
-  }
-
-  static Future<ui.Image> _getSizedSvgImage(
-    ui.Image svgImage,
-    Size size,
-  ) async {
-    final double width = size.width * ui.window.devicePixelRatio;
-    final double height = size.height * ui.window.devicePixelRatio;
-
-    final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
-    final Canvas canvas = Canvas(pictureRecorder);
-    final Rect svgRect = Rect.fromLTRB(
-        0.0, 0.0, svgImage.width.toDouble(), svgImage.height.toDouble());
-    final Rect sizedRect =
-        Rect.fromLTRB(0.0, 0.0, width, height); // owr size here
-    canvas.drawImageRect(svgImage, svgRect, sizedRect, Paint());
-    return pictureRecorder
-        .endRecording()
-        .toImage(width.toInt(), height.toInt());
+    String svgString,
+  ) {
+    return SvgStringLoader(svgString).loadBytes(context).then(
+        (ByteData value) =>
+            BitmapDescriptor.fromBytes(value.buffer.asUint8List()));
   }
 }
