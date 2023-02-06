58fa648ac9bf522f89f1938b841b03b5bfbfef4b
dddrop <go@dddrop.dev>
2023-02-06 23:45:12 +0900

    Fix size of bitmap_descriptor

--- lib/utilities/bitmap_descriptor_helper.dart
+++ lib/utilities/bitmap_descriptor_helper.dart
@@ -9,18 +9,26 @@ class BitmapDescriptorHelper {
   static Future<BitmapDescriptor> getBitmapDescriptorFromSvgAsset(
     BuildContext context,
     String svgAssetLink,
+    Size size,
   ) async {
-    return SvgAssetLoader(svgAssetLink).loadBytes(context).then(
-        (ByteData value) =>
-            BitmapDescriptor.fromBytes(value.buffer.asUint8List()));
+    return SvgAssetLoader(svgAssetLink)
+        .loadBytes(context)
+        .then((ByteData value) => BitmapDescriptor.fromBytes(
+              value.buffer.asUint8List(),
+              size: size,
+            ));
   }
 
   static Future<BitmapDescriptor> getBitmapDescriptorFromSvgString(
     BuildContext context,
     String svgString,
+    Size size,
   ) {
     return SvgStringLoader(svgString).loadBytes(context).then(
-        (ByteData value) =>
-            BitmapDescriptor.fromBytes(value.buffer.asUint8List()));
+          (ByteData value) => BitmapDescriptor.fromBytes(
+            value.buffer.asUint8List(),
+            size: size,
+          ),
+        );
   }
 }
